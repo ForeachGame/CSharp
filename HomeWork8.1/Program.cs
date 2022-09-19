423cfcb6510cb18a0a857505b3a407ad9242f042
ForeachGame <alexcheremisinov@gmail.com>
2022-09-19 15:45:21 +0500

    homework 8.1

---
+++ HomeWork8.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HomeWorkHelpers;
+
+namespace HomeWork8._1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Helpers.PrintStudentInfo(8);
+            /*
+            * С помощью рефлексии выведите все свойства структуры DateTime.
+            */
+            Console.WriteLine("С помощью рефлексии выведите все свойства структуры DateTime");
+            DateTime dateTimeNow = DateTime.Now;
+            int i = 1;
+            Type type = typeof(DateTime); 
+            
+            foreach (var propetryInfo in type.GetProperties())
+            {
+                Console.WriteLine($"{i}. " + propetryInfo.Name + ": " + propetryInfo.GetValue(dateTimeNow));
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
